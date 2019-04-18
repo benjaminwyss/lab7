396c8652b454321e76cc71da5119106df7ad55c3
BenjaminWyss <b040w377@ku.edu>
2019-04-18 12:37:38 -0500

    Create framework for Test class and update makefile.

---
+++ Test.cpp
@@ -0,0 +1,14 @@
+/**
+*	@file Test.cpp
+*	@author Benjamin Wyss
+*	@date 4/18/19
+*	@brief A cpp file for Linked List Test Suite class
+*/
+
+#include "Test.h"
+#include "LinkedListOfInts.h"
+
+void Test::runTests()
+{
+  std::cout << "I'm testing!\n";
+}
---
+++ Test.h
@@ -0,0 +1,22 @@
+/**
+*	@file Test.h
+*	@author Benjamin Wyss
+*	@date 4/18/19
+*	@brief A header file for Linked List Test Suite class
+*/
+
+#ifndef TEST_H
+#define TEST_H
+
+#include <iostream>
+#include "LinkedListOfInts.h"
+
+class Test
+{
+  public:
+
+  void runTests();
+
+};
+
+#endif
