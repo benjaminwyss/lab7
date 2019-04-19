2b599bdac43f9ec68e3fc4ee0b4a1b726b41c3fa
BenjaminWyss <b040w377@ku.edu>
2019-04-18 21:03:47 -0500

    Add helper function for manual testing and update buglist.

--- main.cpp
+++ main.cpp
@@ -8,6 +8,16 @@
 #include "LinkedListOfInts.h"
 #include "Test.h"
 
+void printList(LinkedListOfInts list)
+{
+	std::vector<int> vec = list.toVector();
+	for (unsigned int i = 0; i < vec.size(); i++)
+	{
+		std::cout << vec[i] << "\n";
+	}
+	std::cout << "\n";
+}
+
 int main(int argc, char** argv)
 {
 	Test myTester;
